1973b193a1ea298b627db4caff48cde3eec5fdf9
Hantama <83705123+ivantri@users.noreply.github.com>
2021-12-04 16:27:23 +0800

    Update 2021_11_29_015806_create_tb_admins_table.php

--- database/migrations/2021_11_29_015806_create_tb_admins_table.php
+++ database/migrations/2021_11_29_015806_create_tb_admins_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 class CreateTbAdminsTable extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations.asd
      *
      * @return void
      */
